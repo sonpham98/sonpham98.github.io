00292f42cf9bcd71b4d417ed6231e3df06192a73
sonpham98 <phamminhson98@gmail.com>
2019-09-07 13:11:56 +0700

    new
    new

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+  var x = document.getElementById("sidebar-wrapper");
+  if (x.style.top === "-25em") {
+    x.style.top = "0";
+  } else {
+    x.style.top = "-25em";
+  }
+}
